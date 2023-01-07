a03a2daad89a44da8624b0a8671dc35491bedbcc
Anar Sultanov <anar.sultanov@assessio.se>
2023-01-07 12:35:06 +0100

    Add tenant name to token claims

--- src/main/java/dev/sultanov/keycloak/multitenancy/protocol/oidc/mappers/ClaimsFactory.java
+++ src/main/java/dev/sultanov/keycloak/multitenancy/protocol/oidc/mappers/ClaimsFactory.java
@@ -8,11 +8,13 @@ import java.util.Map;
 public class ClaimsFactory {
 
     private static final String TENANT_ID_KEY = "tenant_id";
+    private static final String TENANT_NAME_KEY = "tenant_name";
     private static final String ROLES_KEY = "roles";
 
     static Map<String, Object> toClaim(TenantMembershipModel membership) {
         Map<String, Object> claim = new HashMap<>();
         claim.put(TENANT_ID_KEY, membership.getTenant().getId());
+        claim.put(TENANT_NAME_KEY, membership.getTenant().getName());
         claim.put(ROLES_KEY, membership.getRoles());
         return Collections.unmodifiableMap(claim);
     }
