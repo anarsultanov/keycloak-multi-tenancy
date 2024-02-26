7eed2c77243d4166a78c4accf7ce5f6ed9486c24
Anar Sultanov <anar.sultanov@assessio.se>
2024-02-26 20:34:04 +0100

    docs: Remove unnecessary types from OpenAPI documentation

--- src/main/java/dev/sultanov/keycloak/multitenancy/resource/representation/TenantMembershipRepresentation.java
+++ src/main/java/dev/sultanov/keycloak/multitenancy/resource/representation/TenantMembershipRepresentation.java
@@ -2,7 +2,9 @@ package dev.sultanov.keycloak.multitenancy.resource.representation;
 
 import java.util.Set;
 import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.media.SchemaProperty;
 import org.keycloak.representations.idm.UserRepresentation;
 
 @Schema
@@ -12,7 +14,18 @@ public class TenantMembershipRepresentation {
     @Schema(readOnly = true)
     private String id;
 
-    @Schema(readOnly = true, description = "Returns basic user info only including id, username, creation date, names, email, enabled state, email verification and federation link.")
+    @Schema(readOnly = true,
+            properties = {
+            @SchemaProperty(name = "id", type = SchemaType.STRING),
+            @SchemaProperty(name = "createdTimestamp", type = SchemaType.NUMBER),
+            @SchemaProperty(name = "username", type = SchemaType.STRING),
+            @SchemaProperty(name = "enabled", type = SchemaType.BOOLEAN),
+            @SchemaProperty(name = "firstName", type = SchemaType.STRING),
+            @SchemaProperty(name = "lastName", type = SchemaType.STRING),
+            @SchemaProperty(name = "email", type = SchemaType.STRING),
+            @SchemaProperty(name = "emailVerified", type = SchemaType.BOOLEAN),
+            @SchemaProperty(name = "federationLink", type = SchemaType.STRING)
+    })
     private UserRepresentation user;
 
     @Schema(required = true)
