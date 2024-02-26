e0629ff53090a8c8163f1c165cc753cd59ea4b58
Anar Sultanov <anar.sultanov@assessio.se>
2024-02-26 19:39:05 +0100

    docs: Update user field description

--- src/main/java/dev/sultanov/keycloak/multitenancy/resource/representation/TenantMembershipRepresentation.java
+++ src/main/java/dev/sultanov/keycloak/multitenancy/resource/representation/TenantMembershipRepresentation.java
@@ -12,7 +12,7 @@ public class TenantMembershipRepresentation {
     @Schema(readOnly = true)
     private String id;
 
-    @Schema(readOnly = true)
+    @Schema(readOnly = true, description = "Returns basic user info only including id, username, creation date, names, email, enabled state, email verification and federation link.")
     private UserRepresentation user;
 
     @Schema(required = true)
